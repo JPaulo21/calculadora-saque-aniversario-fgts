f016532189b870bed2940bdedd3f20e03dd525f7
João Paulo <jp.sport17@gmail.com>
2025-11-08 15:25:26 -0300

    Initial commit

---
+++ src/main/java/com/jp/calculadora_saque_aniversario_fgts/CalculadoraSaqueAniversarioFgtsApplication.java
@@ -0,0 +1,13 @@
+package com.jp.calculadora_saque_aniversario_fgts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSaqueAniversarioFgtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSaqueAniversarioFgtsApplication.class, args);
+	}
+
+}

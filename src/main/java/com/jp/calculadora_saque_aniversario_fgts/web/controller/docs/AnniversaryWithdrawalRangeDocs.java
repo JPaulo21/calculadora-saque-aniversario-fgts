922bfda7e86714c2b0e4f28c1c5b686bb8467e41
João Paulo <jp.sport17@gmail.com>
2026-03-01 08:53:07 -0300

    docs: Atualizar a documentação da API para a tabela de saques-aniversário com exemplos de resposta

--- src/main/java/com/jp/calculadora_saque_aniversario_fgts/web/controller/docs/AnniversaryWithdrawalRangeDocs.java
+++ src/main/java/com/jp/calculadora_saque_aniversario_fgts/web/controller/docs/AnniversaryWithdrawalRangeDocs.java
@@ -12,10 +12,57 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Tag(name = "Tabela do saque-aniversário do FGTS", description = "A tabela organiza os saques por faixas de saldo")
 public interface AnniversaryWithdrawalRangeDocs {
 
     @Operation(summary = "")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = AnniversaryWithdrawalRangeResponseDTO.class)))
+    @ApiResponse(responseCode = "200",
+            content = @Content(mediaType = APPLICATION_JSON_VALUE,
+                    schema = @Schema(type = "string", example = """
+                            {
+                                  "initialRange": 0,
+                                  "rangeLimit": 500,
+                                  "rate": 50,
+                                  "fixedAdditionalAmount": 0
+                                },
+                                {
+                                  "initialRange": 500.01,
+                                  "rangeLimit": 1000,
+                                  "rate": 40,
+                                  "fixedAdditionalAmount": 50
+                                },
+                                {
+                                  "initialRange": 1000.01,
+                                  "rangeLimit": 5000,
+                                  "rate": 30,
+                                  "fixedAdditionalAmount": 150
+                                },
+                                {
+                                  "initialRange": 5000.01,
+                                  "rangeLimit": 10000,
+                                  "rate": 20,
+                                  "fixedAdditionalAmount": 650
+                                },
+                                {
+                                  "initialRange": 10000.01,
+                                  "rangeLimit": 15000,
+                                  "rate": 15,
+                                  "fixedAdditionalAmount": 1150
+                                },
+                                {
+                                  "initialRange": 15000.01,
+                                  "rangeLimit": 20000,
+                                  "rate": 10,
+                                  "fixedAdditionalAmount": 1900
+                                },
+                                {
+                                  "initialRange": 20000.01,
+                                  "rangeLimit": 99999999,
+                                  "rate": 5,
+                                  "fixedAdditionalAmount": 2900
+                                }
+                            """)))
     ResponseEntity<Page<AnniversaryWithdrawalRangeResponseDTO>> getRanges(@PageableDefault Pageable pageable);
 }

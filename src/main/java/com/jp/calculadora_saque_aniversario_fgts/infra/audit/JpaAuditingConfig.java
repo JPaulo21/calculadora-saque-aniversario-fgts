f0ac7e739519934da2613dca8aeab6a779df8936
João Paulo <jp.sport17@gmail.com>
2026-05-23 15:56:55 -0300

    feat: Add JPA auditing configuration for entity auditing support

---
+++ src/main/java/com/jp/calculadora_saque_aniversario_fgts/infra/audit/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.jp.calculadora_saque_aniversario_fgts.infra.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}

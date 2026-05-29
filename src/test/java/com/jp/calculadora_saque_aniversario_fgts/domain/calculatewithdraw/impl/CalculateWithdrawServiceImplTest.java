f97f8e2e670a659f7120f0252c0b89e678ff6950
João Paulo <jp.sport17@gmail.com>
2026-05-29 18:17:11 -0300

    tests: calculation of the anniversary withdrawal

---
+++ src/test/java/com/jp/calculadora_saque_aniversario_fgts/domain/calculatewithdraw/impl/CalculateWithdrawServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.jp.calculadora_saque_aniversario_fgts.domain.calculatewithdraw.impl;
+
+import com.jp.calculadora_saque_aniversario_fgts.domain.anniversarywithdrawal.AnniversaryWithdrawalRange;
+import com.jp.calculadora_saque_aniversario_fgts.domain.anniversarywithdrawal.AnniversaryWithdrawalRangeService;
+import com.jp.calculadora_saque_aniversario_fgts.domain.withdraw.WithdrawInquiry;
+import com.jp.calculadora_saque_aniversario_fgts.domain.withdraw.WithdrawInquiryService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CalculateWithdrawServiceImplTest {
+
+    @Mock
+    private AnniversaryWithdrawalRangeService anniversaryWithdrawalRangeService;
+
+    @Mock
+    private WithdrawInquiryService withdrawInquiryService;
+
+    @InjectMocks
+    private CalculateWithdrawServiceImpl calculateWithdrawService;
+
+    @Test
+    void calculateWithdraw_WithValidData_returnsWithdrawInquiry() {
+        ArgumentMatcher<BigDecimal> isPositive = value -> value.compareTo(BigDecimal.ZERO) > 0;
+        var rate = BigDecimal.valueOf(20);
+        var valorDeConsulta = BigDecimal.valueOf(10000);
+        AnniversaryWithdrawalRange range = AnniversaryWithdrawalRange.builder()
+                .initialRange(BigDecimal.valueOf(5000.01))
+                .rangeLimit(BigDecimal.valueOf(10000))
+                .fixedAdditionalAmount(BigDecimal.valueOf(650))
+                .rate(rate)
+                .build();
+
+        when(anniversaryWithdrawalRangeService.getRangeByValue(argThat(isPositive)))
+                .thenReturn(range);
+
+        WithdrawInquiry sut = calculateWithdrawService.calculateWithdraw(valorDeConsulta);
+
+        verify(withdrawInquiryService).register(any(WithdrawInquiry.class));
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getWithdrawValue()).isEqualTo(BigDecimal.valueOf(2650));
+        assertThat(sut.getFgtsValue()).isEqualTo(BigDecimal.valueOf(10000));
+        assertThat(sut.getFgtsValueAfterWithdraw()).isEqualTo(BigDecimal.valueOf(7350));
+    }
+}
